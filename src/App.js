cf0acbbbd4f5626d22a280f08775d16413855e9b
ItzayHR <k3vin1tz1@gmail.com>
2022-06-03 14:25:32 -0700

    Intentando hacer el deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import '../src/assets/css/App.css';
-import React, { useState, useEffect} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';  
+import React, { useState, useEffect} from 'react'; 
 import Header from './components/Header';
 import Juegos from './components/Juegos';
 import Carrito from './components/Carrito';
--- src/components/Carrito.js
+++ src/components/Carrito.js
@@ -1,6 +1,5 @@
 import React from "react";
 import '../assets/css/Carrito.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
 
 const Carrito = (props) =>
